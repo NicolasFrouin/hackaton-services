dcf70d5a8e40e63b70972f2c0d3ef78490b5d9d2
NicolasFrouin <frouinnicolas@gmail.com>
2025-06-24 13:08:30 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className='flex items-center justify-center h-screen bg-gray-100'>
+      <h1>
+        Hackaton <a href='https://services.ceo'>Services.ceo</a>
+      </h1>
+    </div>
+  );
+}
